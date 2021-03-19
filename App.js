b3d62ed9c95e771f2a2eb7e7f5e31bae767c622b
Stella Martin <stellam777@gmail.com>
2021-03-19 10:49:35 -0400

    fixed Text string issue

--- App.js
+++ App.js
@@ -20,23 +20,26 @@
 //   },
 // });
 
-
 import 'react-native-gesture-handler';
-import React, { useEffect, useState } from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
-import {decode, encode} from 'base-64'
-if (!global.btoa) {  global.btoa = encode }
-if (!global.atob) { global.atob = decode }
-import { firebase } from './src/firebase/config'
+import React, { useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens';
+import { Text } from 'react-native';
+import { decode, encode } from 'base-64';
+if (!global.btoa) {
+  global.btoa = encode;
+}
+if (!global.atob) {
+  global.atob = decode;
+}
+import { firebase } from './src/firebase/config';
 
 const Stack = createStackNavigator();
 
 export default function App() {
-
-  const [loading, setLoading] = useState(true)
-  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
 
   // return (
   //   <NavigationContainer>
@@ -57,53 +60,55 @@ export default function App() {
 
   useEffect(() => {
     const usersRef = firebase.firestore().collection('users');
-    firebase.auth().onAuthStateChanged(user => {
+    firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         usersRef
           .doc(user.uid)
           .get()
           .then((document) => {
-            const userData = document.data()
-            setUser(userData)
-            setLoading(false)
+            const userData = document.data();
+            setUser(userData);
+            setLoading(false);
           })
           .catch((error) => {
-            setLoading(false)
+            setLoading(false);
           });
       } else {
-        setLoading(false)
+        setLoading(false);
       }
     });
   }, []);
 
-  if (loading) {	
-    return (	
-      <>Loading</>	
-    )	
+  if (loading) {
+    return (
+      <>
+        <Text>Loading</Text>
+      </>
+    );
   }
 
   if (!loading) {
-    console.log('This is the user')
-    console.log(user)
+    console.log('This is the user');
+    console.log(user);
   }
 
   return (
     <NavigationContainer>
       <Stack.Navigator /*initialRouteName={user ? "Home" : "Login"}*/>
-        { user ?
+        {user ? (
           <>
-          <Stack.Screen name="Home">
-            {props => <HomeScreen {...props} extraData={user} />}
-          </Stack.Screen>
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Registration" component={RegistrationScreen} />
+            <Stack.Screen name='Home'>
+              {(props) => <HomeScreen {...props} extraData={user} />}
+            </Stack.Screen>
+            <Stack.Screen name='Login' component={LoginScreen} />
+            <Stack.Screen name='Registration' component={RegistrationScreen} />
           </>
-         : (
+        ) : (
           <>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Registration" component={RegistrationScreen} />
-            <Stack.Screen name="Home">
-              {props => <HomeScreen {...props} extraData={user} />}
+            <Stack.Screen name='Login' component={LoginScreen} />
+            <Stack.Screen name='Registration' component={RegistrationScreen} />
+            <Stack.Screen name='Home'>
+              {(props) => <HomeScreen {...props} extraData={user} />}
             </Stack.Screen>
           </>
         )}
