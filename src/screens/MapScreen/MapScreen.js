8f855a0d636241555bc52d4a04c7e10b2fcb3321
Mark Eggensperger <markeggensperger@gmail.com>
2021-03-22 17:50:16 -0400

    feat: tap to zoom to all markers and radius around center of markers

--- src/screens/MapScreen/MapScreen.js
+++ src/screens/MapScreen/MapScreen.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { Component, useState, useEffect, useRef } from 'react';
 import {
   Platform,
   StyleSheet,
@@ -10,11 +10,13 @@ import MapView, {
   PROVIDER_GOOGLE,
   AnimateRegion,
   Marker,
+  Circle,
 } from 'react-native-maps';
 import * as Permission from 'expo-permissions';
 import { firebase } from '../../firebase/config';
 
 export default function MapScreen(props) {
+  let mapRef = useRef(null);
   const sessionRef = firebase.firestore().collection('sessions');
   const usersRef = firebase.firestore().collection('users');
   const [region, setRegion] = useState({
@@ -23,6 +25,11 @@ export default function MapScreen(props) {
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   });
+  const [center, setCenter] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+  });
+  const [notZoomed, setNotZoomed] = useState(true);
   const [sessionUsers, setSessionUsers] = useState([]);
   const colors = ['red', 'green', 'purple'];
   const getCurrentLocation = () => {
@@ -42,24 +49,54 @@ export default function MapScreen(props) {
         maximumAge: 1000,
       }
     );
+    console.log('current region: ', region);
   };
   const goToInitialRegion = () => {
+    // console.log('mapRef: ', mapRef);
     let initialRegion = Object.assign({}, region);
+    // setRegion({ ...region, latitudeDelta: 0.005, longitudeDelta: 0.005 });
     initialRegion['latitudeDelta'] = 0.005;
     initialRegion['longitudeDelta'] = 0.005;
-    mapView.animateToRegion(initialRegion, 2000);
+    console.log('session users: ', sessionUsers);
+    console.log(
+      'attempting animate: ',
+      sessionUsers.map((user) => ({
+        latitude: user.location.latitude,
+        longitude: user.location.longitude,
+      }))
+    );
+    mapRef.current.fitToCoordinates(
+      sessionUsers.map((user) => ({
+        latitude: user.location.latitude,
+        longitude: user.location.longitude,
+      })),
+      { animated: true }
+    );
+    console.log('animate complete');
   };
   const getSessionLocations = () => {
     usersRef.where('session', '==', '123456').onSnapshot(
       (querySnapshot) => {
         const activeUsers = [];
+        let lats = 0;
+        let longs = 0;
         querySnapshot.forEach((doc) => {
           const user = doc.data();
           user.id = doc.id;
           activeUsers.push(user);
           console.log('session user: ', user);
+          lats += user.location.latitude;
+          longs += user.location.longitude;
         });
         setSessionUsers(activeUsers);
+        let latAvg = lats / activeUsers.length;
+        let longAvg = longs / activeUsers.length;
+        setCenter({
+          latitude: latAvg,
+          longitude: longAvg,
+        });
+        if (notZoomed) goToInitialRegion(activeUsers);
+        setNotZoomed(false);
       },
       (error) => {
         console.log(error);
@@ -68,7 +105,7 @@ export default function MapScreen(props) {
   };
   useEffect(() => {
     getCurrentLocation();
-  }, []);
+  }, [region, mapRef]);
   useEffect(() => {
     getSessionLocations();
   }, []);
@@ -79,9 +116,11 @@ export default function MapScreen(props) {
       showsUserLocation={true}
       followUserLocation={true}
       zoomEnabled={true}
-      // ref={(ref) => (this.mapView = ref)}
-      // onMapReady={goToInitialRegion()}
+      ref={mapRef}
+      // ref={(ref) => (mapRef = ref)}
+      // onMapReady={mapReady}
       initialRegion={region}
+      onPress={goToInitialRegion}
     >
       {sessionUsers.map((user, idx) => {
         return (
@@ -93,9 +132,19 @@ export default function MapScreen(props) {
             }}
             title={user.fullName}
             pinColor={colors[idx]}
-          />
+          >
+            <View style={{ padding: 10 }}>
+              <Text style={{ color: colors[idx] }}>
+                {user.fullName
+                  .split(' ')
+                  .map((name) => name[0])
+                  .join('')}
+              </Text>
+            </View>
+          </Marker>
         );
       })}
+      <Circle center={center} radius={15000} fillColor='rgba(20,20,240,0.1)' />
     </MapView>
   );
 }
