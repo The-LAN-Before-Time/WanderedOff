17f620de5c12d62d08e7f799c93424b2fd2018ca
Stella Martin <stellam777@gmail.com>
2021-03-25 18:59:35 -0400

    update: played with geofirex and watchPosition

--- src/screens/LocationSharingScreen/LocationSharingScreen.js
+++ src/screens/LocationSharingScreen/LocationSharingScreen.js
@@ -21,70 +21,93 @@ import haversine from 'haversine';
 
 import 'firebase/firestore';
 import 'firebase/auth';
-
+// import { useCollectionData } from 'react-firebase-hooks/firestore';
 import * as geofirex from 'geofirex';
+// import Geolocation from "@react-native-community/geolocation";
+
 const geo = geofirex.init(firebase);
 
 export default function LocationSharing(props) {
   const firestore = firebase.firestore();
   const userLocationRef = firestore.collection('sessionLocations');
-  const lastPositionRef = userLocationRef.where('sessionId', '==', '123456').orderBy('createdAt').limit(5);
-  const geoQuery = geo.query(lastPositionRef)
+  const lastPositionRef = userLocationRef
+    .where('sessionId', '==', '123457')
+    .orderBy('createdAt', 'desc')
+    .limit(5);
+
+  const geoQuery = geo.query(lastPositionRef);
+  const selfLocation = lastPositionRef.where('userId', "==", props.extraData.id);
+
   // geoQuery.subscribe(locations => {
   //   console.log('This is our subscription')
   //   console.log(locations)
   // })
-  let subscribed = false
+  let [subscribed, setSubscribed] = useState(false);
   let myPosition = false;
+  // const [myPosition, setMyPosition] = useState(null);
   const [lastSent, setLastSent] = useState(1616612900920);
   const [running, toggle] = useState(true);
   const [locationSent, setLocationSent] = useState(false);
+
   console.log('page rendering');
+
 
   const sendLocation = () => {
     console.log('attempting to send location');
     console.log('my location: ', myPosition);
     if (myPosition) {
-      const point = geo.point(myPosition.latitude, myPosition.longitude)
+      //const point = geo.point(myPosition.latitude, myPosition.longitude);
       console.log('send ing');
       userLocationRef.add({
         createdAt: firebase.firestore.FieldValue.serverTimestamp(),
         sessionId: '123456',
         userId: props.extraData.id,
-        point
+        location: myPosition,
       });
       console.log('sent');
     }
   };
 
-  function getPosition() {
-    if (running) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        console.log('new position: ', position);
-        // console.log(
-        //   'distance: ',
-        //   haversine(myPosition, position.coords, { unit: 'meter' }),
-        //   ' meters'
-        // );
-        myPosition = {
-          latitude: parseFloat(position.coords.latitude),
-          longitude: parseFloat(position.coords.longitude),
-        };
-        console.log('position set');
-        if (!subscribed) {
-          geoQuery.within(geo.point(myPosition.latitude, myPosition.longitude), 100, 'position').subscribe(locations => {
-            console.log('This is our subscription')
-            console.log(locations)
-          })
-        }
-        sendLocation();
-      },
-      (error) => console.log(error.message),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-    );
-    };
-  }
+  // function getPosition() {
+  //   if (running) {
+  //     navigator.geolocation.getCurrentPosition(
+  //       (position) => {
+  //         console.log('new position: ', position);
+  //         // console.log(
+  //         //   'distance: ',
+  //         //   haversine(myPosition, position.coords, { unit: 'meter' }),
+  //         //   ' meters'
+  //         // );
+  //         myPosition = {
+  //           latitude: parseFloat(position.coords.latitude),
+  //           longitude: parseFloat(position.coords.longitude),
+  //         };
+  //         //userLocationRef.setPoint("5iPi91px6uRxI6fezwSu", myPosition.latitude, myPosition.longitude);
+  //         const point = geo.point(myPosition.latitude, myPosition.longitude);
+  //         // geo.query(selfLocation).setDoc('myLocation', { point: point.data, userId: props.extraData.id, sessionId: "1234567", createdAt: firebase.firestore.FieldValue.serverTimestamp() });
+  //         console.log('position set');
+  //         // if (!subscribed) {
+  //         console.log("setting up subscription")
+  //         // const observer = lastPositionRef.onSnapshot(querySnapshot => {console.log("OBSERVER", querySnapshot.size)}, err => { console.log("err:", err) });
+  //           // geoQuery
+  //           //   .within(
+  //           //     geo.point(myPosition.latitude, myPosition.longitude),
+  //           //     1000,
+  //           //     'point', { log: true }
+  //           //   )
+  //           //   .subscribe((locations) => {
+  //           //     console.log('This is our subscription');
+  //           //     console.log(locations);
+  //           //   });
+  //         //     setSubscribed(true);
+  //         // }
+  //         //sendLocation();
+  //       },
+  //       (error) => console.log(error.message),
+  //       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+  //     );
+  //   }
+  // }
 
   // let watchID = navigator.geolocation.watchPosition(
   //   (position) => {
@@ -100,11 +123,40 @@ export default function LocationSharing(props) {
   //   { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
   // );
 
+  // useEffect(() => {
+  //   const observer = lastPositionRef.onSnapshot(querySnapshot => {console.log("OBSERVER", querySnapshot.size)}, err => { console.log("err:", err) });
+  //   // const interval = setInterval(getPosition, 3000);
+  //   // return () => clearInterval(interval);
+  //   return () => observer();
+  // }, []);
 
-  useEffect(() => {
-    const interval = setInterval(getPosition, 3000);
-    return () => clearInterval(interval);
+ useEffect(() => {
+    const watchId = navigator.geolocation.watchPosition(
+      pos => {
+        console.log("WATCH POSITION", pos)
+        if(myPosition) {
+          console.log(
+            'distance: ',
+            haversine(myPosition, pos.coords, { unit: 'meter' }),
+            ' meters'
+          );
+        }
+        myPosition = {
+          latitude: pos.coords.latitude,
+          longitude: pos.coords.longitude
+        };
+        sendLocation();
+      },
+      e => console.log(e.message), { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 0 }
+    );
+    return () => Geolocation.clearWatch(watchId);
   }, []);
+
+  // useEffect(() => {
+  //   sendLocation();
+  // }, [myPosition])
+
+
 
   function toggleRunning(status) {
     // if (!status) getPosition();
