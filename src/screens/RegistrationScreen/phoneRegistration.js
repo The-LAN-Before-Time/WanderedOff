74be04fba073f1a6a11ce8bc90f7def14a184c45
Mark Onofrio <hello@markonofrio.com>
2021-03-29 22:51:30 -0400

    Feat: authenticat phonenumber with expo-firebase-recaptcha

--- src/screens/RegistrationScreen/phoneRegistration.js
+++ src/screens/RegistrationScreen/phoneRegistration.js
@@ -1,25 +1,35 @@
-import React, {useState, useRef} from 'react'
-import {Image, Text, TextInput, TouchableOpacity, View, StyleSheet, SafeAreaView, } from 'react-native'
+import React, { useState, useEffect, useRef } from 'react';
+import {
+    Text,
+    View,
+    TextInput,
+    Button,
+    StyleSheet,
+    TouchableOpacity,
+    Platform,
+} from 'react-native';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import styles from './styles';
 import {firebase} from '../../firebase/config'
 
-
+import { FirebaseRecaptchaVerifierModal, FirebaseRecaptchaBanner } from 'expo-firebase-recaptcha';
 
 function PhoneAuthScreen() {
-    const [phone, setPhone] = useState('')
-    const [confirmResult, setConfirmResult] = useState(null)
-    const [verificationCode, setVerificationCode] = useState('')
-    const [userId, setUserId] = useState('')
-    const buttonId = useRef()
 
-    const appVerifier = new firebase.auth.RecaptchaVerifier(buttonId, {
-        'size': 'invisible',
-        'callback': (response) => {
-            // reCAPTCHA solved, allow signInWithPhoneNumber.
-            onSignInSubmit();
-        }
-    });
+    const recaptchaVerifier = useRef(null);
+    const [phoneNumber, setPhoneNumber] = useState();
+    const [verificationId, setVerificationId] = useState();
+    const [verificationCode, setVerificationCode] = useState();
+    const firebaseConfig = firebase.apps.length ? firebase.app().options : undefined;
+    const [message, showMessage] = useState(
+        !firebaseConfig || Platform.OS === 'web'
+            ? {
+                text:
+                    'To get started, provide a valid firebase config in App.js and open this snack on an iOS or Android device.',
+            }
+            : undefined
+    );
+    const attemptInvisibleVerification = true;
 
     const validatePhoneNumber = () => {
         console.log("Validate Phone Number")
@@ -28,141 +38,92 @@ function PhoneAuthScreen() {
         return true
     }
 
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            backgroundColor: '#aaa'
-        },
-        page: {
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center'
-        },
-        textInput: {
-            marginTop: 20,
-            width: '90%',
-            height: 40,
-            borderColor: '#555',
-            borderWidth: 2,
-            borderRadius: 5,
-            paddingLeft: 10,
-            color: '#fff',
-            fontSize: 16
-        },
-        themeButton: {
-            width: '90%',
-            height: 50,
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: '#888',
-            borderColor: '#555',
-            borderWidth: 2,
-            borderRadius: 5
-        },
-        themeButtonTitle: {
-            fontSize: 24,
-            fontWeight: 'bold',
-            color: '#fff'
-        },
-        verificationView: {
-            width: '100%',
-            alignItems: 'center',
-            marginTop: 50
-        }
-    })
-
-    const handleSendCode = () => {
-        // Request to send OTP (over the phone)
-        if (validatePhoneNumber()) {
-            firebase
-                .auth()
-                .signInWithPhoneNumber(phone, appVerifier)
-                .then(result => {
-                    setConfirmResult(result)
-                })
-                .catch(error => {
-                    alert(error.message)
-                    console.log(error)
-                })
-        } else {
-            alert('Invalid Phone Number')
-        }
-    }
-
-    const handleVerifyCode = () => {
-        // Request for OTP verification
-        if (verificationCode.length === 6) {
-            confirmResult
-                .confirm(verificationCode)
-                .then(user => {
-                    setUserId(user.uid)
-                    alert(`Verified! ${user.uid}`)
-                })
-                .catch(error => {
-                    alert(error.message)
-                    console.log(error)
-                })
-        } else {
-            alert('Please enter a 6 digit OTP code.')
-        }
-    }
-
-    const renderConfirmationCodeView = () => {
-        return (
-            <View style={styles.verificationView}>
-                <TextInput
-                    style={styles.textInput}
-                    placeholder='Verification code'
-                    placeholderTextColor='#eee'
-                    value={verificationCode}
-                    keyboardType='numeric'
-                    onChangeText={verificationCode => {
-                        setVerificationCode(verificationCode)
-                    }}
-                    maxLength={6}
-                />
-                <TouchableOpacity
-                    style={[styles.themeButton, {marginTop: 20}]}
-                    onPress={handleVerifyCode}>
-                    <Text style={styles.themeButtonTitle}>Verify Code</Text>
-                </TouchableOpacity>
-            </View>
-        )
-    }
-
-    return (
-        <SafeAreaView style={[styles.container, {backgroundColor: '#333'}]}>
-            <View style={styles.page}>
-                <TextInput
-                    style={styles.textInput}
-                    placeholder='Phone Number with country code'
-                    placeholderTextColor='#eee'
-                    keyboardType='phone-pad'
-                    value={phone}
-                    onChangeText={phone => {
-                        setPhone(phone)
-                    }}
-                    maxLength={15}
-                    editable={confirmResult ? false : true}
-                />
-
-                <TouchableOpacity
-                    style={[styles.themeButton, {marginTop: 20}]}
-                    onPress={
-                        confirmResult
-                            ? changePhoneNumber
-                            : handleSendCode
+    return(
+        <View style={{ padding: 20, marginTop: 50 }}>
+            <FirebaseRecaptchaVerifierModal
+                ref={recaptchaVerifier}
+                firebaseConfig={firebaseConfig}
+                attemptInvisibleVerification={attemptInvisibleVerification}
+            />
+            <Text style={{ marginTop: 20 }}>Enter phone number</Text>
+            <TextInput
+                style={{ marginVertical: 10, fontSize: 17 }}
+                placeholder="+1 999 999 9999"
+                autoFocus
+                autoCompleteType="tel"
+                keyboardType="phone-pad"
+                textContentType="telephoneNumber"
+                onChangeText={phoneNumber => setPhoneNumber(phoneNumber)}
+            />
+            <Button
+                title="Send Verification Code"
+                disabled={!phoneNumber}
+                onPress={async () => {
+                    /** The FirebaseRecaptchaVerifierModal ref implements the
+                     *  FirebaseAuthApplicationVerifier interface and can be
+                     *  passed directly to `verifyPhoneNumber`.
+                     */
+                    try {
+                        const phoneProvider = new firebase.auth.PhoneAuthProvider();
+                        const cleanPhoneNumber =  phoneNumber.replace(/[^\d]/g, '')
+                        const verificationId = await phoneProvider.verifyPhoneNumber(
+                            `+${cleanPhoneNumber}`,
+                            recaptchaVerifier.current
+                        );
+                        setVerificationId(verificationId);
+                        showMessage({
+                            text: 'Verification code has been sent to your phone.',
+                        });
+                    } catch (err) {
+                        showMessage({ text: `Error: ${err.message} ${phoneNumber}`, color: 'red' });
                     }
-                    ref={buttonId}
-                >
-                    <Text style={styles.themeButtonTitle}>
-                        {confirmResult ? 'Change Phone Number' : 'Send Code'}
+                }}r
+            />
+            <Text style={{ marginTop: 20 }}>Enter Verification code</Text>
+            <TextInput
+                style={{ marginVertical: 10, fontSize: 17 }}
+                editable={!!verificationId}
+                placeholder="123456"
+                onChangeText={setVerificationCode}
+            />
+            <Button
+                title="Confirm Verification Code"
+                disabled={!verificationId}
+                onPress={async () => {
+                    try {
+                        const credential = firebase.auth.PhoneAuthProvider.credential(
+                            verificationId,
+                            verificationCode
+                        );
+                        await firebase.auth().signInWithCredential(credential);
+                        showMessage({ text: 'Phone authentication successful 👍' });
+                    } catch (err) {
+                        showMessage({ text: `Error: ${err.message} ${typeof phoneNumber}`, color: 'darkred' });
+                    }
+                }}
+            />
+            {message ? (
+                <TouchableOpacity
+                    style={[
+                        StyleSheet.absoluteFill,
+                        { backgroundColor: 0xffffffee, justifyContent: 'center' },
+                    ]}
+                    onPress={() => showMessage(undefined)}>
+                    <Text
+                        style={{
+                            color: message.color || 'blue',
+                            fontSize: 17,
+                            textAlign: 'center',
+                            margin: 20,
+                        }}>
+                        {message.text}
                     </Text>
                 </TouchableOpacity>
-
-                {confirmResult ? renderConfirmationCodeView() : null}
-            </View>
-        </SafeAreaView>
+            ) : (
+                undefined
+            )}
+            {attemptInvisibleVerification && <FirebaseRecaptchaBanner />}
+        </View>
     )
 }
 
